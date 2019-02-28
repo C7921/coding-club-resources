9f3c61f5777290e0e455a18178ac21ac3bb077b6
C7921 <c.ellemsanders@gmail.com>
2019-02-28 18:48:20 +1100

    Inital Repo Deploy

---
+++ Tello Drone/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+python3 tello3.py
+
+command
+
---
+++ pwPicker.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import random
+import string
+
+adjectives = ['sleepy', 'slow', 'smelly', 'wet', 'fat', 'green', 'red', 'orange', 'yellow', 'purple']
+
+nouns = ['apple', 'dinosaur', 'ball', 'dragon', 'goat', 'duck', 'panda', 'hammer']
+
+
+print("Welcome to Password Picker")
+
+while True:
+
+    for num in range(3):
+        adjective = random.choice(adjectives)
+        noun =  random.choice(nouns)
+        number = random.randrange(0, 100)
+        special_char = random.choice(string.punctuation)
+
+        password = adjective + noun + str(number) + special_char
+        print("Your new password is: %s" % password)
+
+    response = input("Would you like another password? Type y or n: ")
+    if response == "n":
+        break
